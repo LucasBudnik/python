d8705d16247ccd2a38caf237ebad8d6f16c29c45
lbudnik <lucasbudnik@hotmail.com.ar>
2024-12-09 17:48:32 -0300

    add clase 11

---
+++ clase11_problema1.py
@@ -0,0 +1,21 @@
+# Gestión de Descuentos
+
+'''
+Imaginá que en tu tienda querés implementar un sistema de descuentos automáticos. Vas a desarrollar un
+programa que permita calcular el precio final de un producto después de aplicar un descuento. Para hacerlo:
+
+1. Crea una función que reciba como parámetros 
+
+recibe
+el precio original del producto y 
+el porcentaje de descuento, 
+
+y que retorne 
+el precio final con el descuento aplicado.
+
+2. Luego, pedí que se ingrese el precio y el porcentaje de descuento. 
+Mostrá el precio final después de aplicar el descuento.
+'''
+
+# 
+def 
